6b80508c65306a208d1746c15ad4b01d27f11fe3
jmil <j.mil@wp.pl>
2024-12-24 23:58:00 +0100

    Updated visit

--- app/routes/visit.py
+++ app/routes/visit.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, flash
 from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
+
+from app import db
+from app.models import Procedure
 
 visit_bp = Blueprint('visit', __name__)
 
@@ -9,6 +12,18 @@ visit_bp = Blueprint('visit', __name__)
 @login_required
 def main_form():
     try:
-        return render_template('visit.html', user=current_user.login)
+        zabiegi = db.session.query(Procedure).all()
+        return render_template('visit.html', user=current_user.login, zabiegi=zabiegi)
+    except TemplateNotFound:
+        return abort(404)
+
+
+@visit_bp.route('/generuj', methods=['GET'])  # GENERUJ, podstawienie właściwego pliku html
+@login_required
+def generuj():
+    try:
+        pass
+        return "generuj ..."
+
     except TemplateNotFound:
         return abort(404)
