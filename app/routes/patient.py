0efc3b9b8471a8c9a7c9bcc0b1363c8c166b0131
MaciejMilewski <s17947@pjwstk.edu.pl>
2024-12-26 16:06:08 +0100

    Updated visit - adding patient to visit

--- app/routes/patient.py
+++ app/routes/patient.py
@@ -1,15 +1,68 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, request, abort
 from flask_login import current_user, login_required
 from jinja2 import TemplateNotFound
+from sqlalchemy import and_
+
+from app.models import Patient, Visit
+from app import db
+
+from math import ceil
+
+ITEMS_PER_PAGE = 5  # Liczba wyników na stronę
+
 
 patient_bp = Blueprint('patient', __name__)
 
 
-@patient_bp.route('/patient', methods=['GET'])
+@patient_bp.route('/patient', methods=['GET', 'POST'])
 @login_required
 def patient_main():
+    results = None
+    current_page = int(request.args.get('page', 1))  # Domyślnie pierwsza strona
+    total_pages = 1                                  # Domyślnie jedna strona (gdy brak wyników)
     try:
+        if request.method == 'POST':
+            # Pobierz kryteria wyszukiwania
+            first_name = request.form.get('first_name')
+            surname = request.form.get('surname')
+            pesel = request.form.get('pesel')
+            city = request.form.get('city')
+            nfz_info = request.form.get('nfz_info')
+            start_date = request.form.get('start_date')
+            end_date = request.form.get('end_date')
 
-        return render_template('patient.html', user=current_user.login)
+            # Tworzenie dynamicznego filtra
+            filters = []
+            if first_name:
+                filters.append(Patient.first_name.ilike(f"%{first_name}%"))
+            if surname:
+                filters.append(Patient.surname.ilike(f"%{surname}%"))
+            if pesel:
+                filters.append(Patient.pesel == pesel)
+            if city:
+                filters.append(Patient.city.ilike(f"%{city}%"))
+            if nfz_info:
+                filters.append(Visit.nfz_info.ilike(f"%{nfz_info}%"))
+            if start_date and end_date:
+                filters.append(Visit.examination_date.between(start_date, end_date))
+
+            # Zapytanie z paginacją
+            query = db.session.query(Patient).join(Visit).filter(*filters)
+            total_results = query.count()
+            total_pages = ceil(total_results / ITEMS_PER_PAGE)
+            results = query.offset((current_page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE).all()
+
+        return render_template(
+            'patient.html',
+            user=current_user.login,
+            results=results,
+            current_page=current_page,
+            total_pages=total_pages,
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+        return abort(500)
+
     except TemplateNotFound:
         return abort(404)
+
--- app/routes/visit.py
+++ app/routes/visit.py
@@ -1,22 +1,22 @@
-from datetime import date
+from datetime import date, datetime
 from io import BytesIO
-
-from flask import Blueprint, render_template, request, abort, jsonify, send_file, redirect, url_for
+from sqlalchemy.exc import IntegrityError
+from flask import Blueprint, render_template, request, abort, jsonify, send_file, flash, redirect
 from flask_login import login_required, current_user
 
 from jinja2 import TemplateNotFound
 
 from app import db, parquet_data, dane_woj
-from app.models import Procedure, Patient
+from app.models import Procedure, Patient, Visit
 from app.utils.const import place
 from app.utils.parquet_util import get_streets_from_memory
 
 from app.utils.utils import (ogolne_items, orl_items, validate_request_badania, validate_request_structure_main,
                              validate_request_szept, zalecenia, generate_pdf)
 
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
-ALLOWED_FIELDS = {'pesel', 'surname'}  # Definicja dozwolonych kolumn dla szukania w bazie danych
+ALLOWED_FIELDS = {'pesel', 'surname'}  # Definicja dozwolonych kolumn -field- dla szukania w bazie danych
 
 
 def get_data_to_autocomplete(query: str, field: str) -> Optional[List[List]]:
@@ -190,16 +190,149 @@ def drukuj():
     buffer = BytesIO(pdf_data)
     buffer.seek(0)
 
-    # nie działa przekierowanie na visit.main_form
-    redirect(url_for('visit.main_form'))
-
     # Zwrócenie pliku PDF jako odpowiedź
     return send_file(buffer, mimetype='application/pdf',
                      download_name=f"{request.form['first_name']}_{request.form['surname']}.pdf",
                      as_attachment=True)
 
 
+
+@visit_bp.route('/zapis', methods=['POST'])
 @login_required
-@visit_bp.route('/zapis', methods=['POST', 'GET'])
 def zapis_wizyty_do_bazy():
-    pass
+
+    dane = request.form
+
+    gender = 'M' if int(dane.get("pesel")[10]) % 2 else "K"
+
+    # 'hiddenResultInput' - zwraca id pacjenta, który jest już zapisany w tabeli 'patient' - przez autocomplete !
+    hidden_result_input = dane.get("hiddenResultInput", "")
+
+    try:
+        if hidden_result_input == "":
+            # sprawdzić po 'peselu' czy zapisywany aktualnie pacjent jest już w tabeli 'patient' ?
+            result = db.session.execute(db.select(Patient.id).filter(Patient.pesel == dane.get("pesel"))).first()
+
+            # 1. jeżeli NIE, to zapisać jego dane w tabeli 'pacjent' (name,pesel,adres)
+            if result is None:
+                new_patient = Patient(first_name=dane.get("first_name"),
+                                      surname=dane.get("surname"),
+                                      pesel=dane.get("pesel"),
+                                      gender=gender,
+                                      state=dane.get("wojewodztwo"),
+                                      city=dane.get("city_select"),
+                                      street=dane.get("street"),
+                                      apartment_number=dane.get("home_numer"))
+                db.session.add(new_patient)
+                db.session.commit()
+                patient_saved_id = db.session.execute(
+                    db.select(Patient.id).filter(Patient.pesel == dane.get("pesel"))
+                ).scalar()
+                patient_id = patient_saved_id
+            else:
+                patient_id = result[0]
+        else:
+            patient_id = dane.get("hiddenResultInput")
+
+        # Obsługa daty audiogramu i usunięcie zbędnych pól, jeśli brak danych
+        # Inicjalizacja pól audiogramu domyślnie jako None
+        ul_250 = ul_500 = ul_1000 = ul_2000 = ul_3000 = ul_4000 = ul_6000 = ul_8000 = None
+        up_250 = up_500 = up_1000 = up_2000 = up_3000 = up_4000 = up_6000 = up_8000 = None
+        data_audiogramu = None
+
+        if dane.get("data_audiogramu"):
+            # Pobierz dane audiogramu
+            data_audiogramu = datetime.strptime(dane.get("data_audiogramu"), "%Y-%m-%d")
+            ul_250 = dane.get('UL__250')
+            ul_500 = dane.get('UL__500')
+            ul_1000 = dane.get('UL__1000')
+            ul_2000 = dane.get('UL__2000')
+            ul_3000 = dane.get('UL__3000')
+            ul_4000 = dane.get('UL__4000')
+            ul_6000 = dane.get('UL__6000')
+            ul_8000 = dane.get('UL__8000')
+            up_250 = dane.get('UP__250')
+            up_500 = dane.get('UP__500')
+            up_1000 = dane.get('UP__1000')
+            up_2000 = dane.get('UP__2000')
+            up_3000 = dane.get('UP__3000')
+            up_4000 = dane.get('UP__4000')
+            up_6000 = dane.get('UP__6000')
+            up_8000 = dane.get('UP__8000')
+
+            # Sprawdzenie, czy jakakolwiek wartość audiogramu jest niewypełniona
+            czy_audiogram_ma_dane = any([
+                data_audiogramu,
+                ul_250, ul_500, ul_1000, ul_2000, ul_3000, ul_4000, ul_6000, ul_8000,
+                up_250, up_500, up_1000, up_2000, up_3000, up_4000, up_6000, up_8000
+            ])
+
+            if not czy_audiogram_ma_dane:
+                # Jeśli brak danych, zerujemy datę audiogramu i wszystkie pola
+                data_audiogramu = None
+                ul_250 = None
+                ul_500 = None
+                ul_1000 = None
+                ul_2000 = None
+                ul_3000 = None
+                ul_4000 = None
+                ul_6000 = None
+                ul_8000 = None
+                up_250 = None
+                up_500 = None
+                up_1000 = None
+                up_2000 = None
+                up_3000 = None
+                up_4000 = None
+                up_6000 = None
+                up_8000 = None
+
+        # 3. zapisać resztę danych w tabeli 'Visit'
+
+        if patient_id is None:
+            flash("Nie można zapisać wizyty, ponieważ nie udało się znaleźć pacjenta.", "danger")
+            return redirect('/visit')
+
+        print("zabiegi:", dane.get('zabieg2'))
+        new_visit = Visit(
+            user_id=current_user.id,
+            patient_id=patient_id,
+            diagnosis=dane.get('diagnoza'),
+            location=dane.get('siteZapis'),
+            interview=dane.get('wywiad'),
+            general_info=dane.get('ogolne'),
+            orl=dane.get('orl'),
+            examination=dane.get('laryngolog'),
+            recommendations=dane.get('zalecenie'),
+            whisper_test=dane.get('szepty'),
+            nfz_info=dane.get('kody_nfz'),
+            audiogram_date=data_audiogramu,
+            examination_date=date.today(),
+            routine=dane.get('zabiegi'),
+            ul_250=ul_250,
+            ul_500=ul_500,
+            ul_1000=ul_1000,
+            ul_2000=ul_2000,
+            ul_3000=ul_3000,
+            ul_4000=ul_4000,
+            ul_6000=ul_6000,
+            ul_8000=ul_8000,
+            up_250=up_250,
+            up_500=up_500,
+            up_1000=up_1000,
+            up_2000=up_2000,
+            up_3000=up_3000,
+            up_4000=up_4000,
+            up_6000=up_6000,
+            up_8000=up_8000)
+
+        db.session.add(new_visit)
+        db.session.commit()
+        flash("Zapisano pomyślnie dane wizyty w bazie danych!", "success")
+        return redirect('/visit')
+    except IntegrityError as e:
+        db.session.rollback()
+        flash(f"Błąd zapisu bazy danych: {e}", "danger")
+        return redirect('/visit')
+
+
