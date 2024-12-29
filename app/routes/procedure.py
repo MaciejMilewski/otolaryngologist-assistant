34d7e828aafa501c0aad7e31e41beb27c0cd69a1
MaciejMilewski <s17947@pjwstk.edu.pl>
2024-12-29 17:34:00 +0100

    Added procedure

--- app/routes/procedure.py
+++ app/routes/procedure.py
@@ -1,14 +1,181 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, jsonify
 from flask_login import current_user, login_required
+from sqlalchemy.inspection import inspect
 from jinja2 import TemplateNotFound
 
+from app import db
+from app.models import Procedure
+from app.utils.const import W0, W1, W2, W3, W9, W10, W16
+
 procedure_bp = Blueprint('procedure', __name__)
+
+
+# Funkcja pomocnicza do konwersji na słownik
+def object_to_dict(obj):
+    return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
+
+
+# Funkcja czyta do zmiennej globalnej międzynarodową klasyfikację
+# chorób ICD-10
+def get_icd_10():
+    """
+       Reads the contents of the 'instance/icd_10.txt' file line by line and returns it as a list of strings.
+
+       Returns:
+           list: A list of strings where each string represents a line from the file.
+                 Returns an empty list if the file does not exist or cannot be read.
+       """
+    try:
+        with open('instance/icd_10.txt', 'r', encoding="utf-8") as file:
+            get_data_icd_10 = file.readlines()
+    except FileNotFoundError:
+        print("Error: Specified file does not exist.")
+        return []
+    except IOError as e:
+        print(f"Error: Unable to read the file. Details: {e}")
+        return []
+    return get_data_icd_10
+
+
+# Zmienna globalna przechowująca dane ICD-10
+data_icd = get_icd_10()
 
 
 @procedure_bp.route('/procedure', methods=['GET'])
 @login_required
 def procedure_main():
     try:
-        return render_template('procedure.html', user=current_user.login)
+        zabiegi = db.session.query(Procedure).all()
+
+        zabiegi_json = [object_to_dict(zabieg) for zabieg in zabiegi]
+
+        return render_template('procedure.html',
+                               user=current_user.login,
+                               lista_W0=W0,
+                               lista_W1=W1,
+                               lista_W2=W2,
+                               lista_W3=W3,
+                               lista_W9=W9,
+                               lista_W10=W10,
+                               lista_W16=W16,
+                               the_best=2,
+                               data=zabiegi_json
+        )
+    except TemplateNotFound:
+        return abort(404)
+
+
+@procedure_bp.route('/procedure_wynik_nfz', methods=['POST'])
+@login_required
+def procedure_wynik_nfz():
+    try:
+        lista_w1 = request.form.getlist('selektor_W1')
+        lista_w2 = request.form.getlist('selektor_W2')
+        lista_w3 = request.form.getlist('selektor_W3')
+        lista_wx = request.form.getlist('selektor_WX')
+        result_w01 = len(lista_w1)
+        result_w02 = len(lista_w2)
+        result_w03 = len(lista_w3)
+        result_w09 = 0
+        result_w10 = 0
+        result_w16 = 0
+
+        if len(lista_wx) > 0:
+            for i in lista_wx:
+                if int(i) < 100:
+                    result_w09 += 1
+                if (int(i) > 100) and (int(i) < 200):
+                    result_w10 += 1
+                if int(i) > 200:
+                    result_w16 += 1
+        best = 2
+        if result_w01 > 0:
+            best = 3
+        if result_w16 > 0:
+            best = 7
+        if result_w09 > 0 or result_w10 > 0:
+            best = 8
+        if result_w01 > 2 or result_w02 == 1:
+            best = 4
+        if (result_w01 > 2 and result_w02 == 1) or (result_w02 == 1 and result_w16 > 0) or (result_w02 > 1) \
+                or (result_w03 == 1):
+            best = 5
+        if (result_w01 > 2 and result_w03 == 1) or (result_w03 > 1) or (result_w10 > 1):
+            best = 6
+
+        zabiegi = db.session.query(Procedure).all()
+        zabiegi_json = [object_to_dict(zabieg) for zabieg in zabiegi]
+
+        return render_template('procedure.html',
+                               user=current_user.login,
+                               lista_W0=W0,
+                               lista_W1=W1,
+                               lista_W2=W2,
+                               lista_W3=W3,
+                               lista_W9=W9,
+                               lista_W10=W10,
+                               lista_W16=W16,
+                               the_best=best,
+                               data=zabiegi_json
+        )
+    except TemplateNotFound:
+        return abort(404)
+
+
+@procedure_bp.route('/body_mass_index', methods=['POST'])
+@login_required
+def body_mass_index():
+    try:
+        # Pobieranie danych z żądania JSON
+        mass = request.json.get('weight', None)
+        height = request.json.get('height', None)
+
+        # Walidacja obecności danych
+        if mass is None or height is None:
+            return jsonify({'error': 'Dane są wymagane!'}), 400
+
+        try:
+            # Konwersja danych na float
+            index_mass = float(mass)
+            index_height = float(height)
+
+            # Walidacja wartości
+            if index_mass <= 0 or index_height <= 0:
+                return jsonify({'error': 'Wartości muszą być dodatnie!', 'bmi': ' - wszystkie liczby muszą być dodatnie'}), 400
+
+            # Obliczenie BMI
+            bmi = round(index_mass / (index_height * index_height * 0.0001), 1)
+            return jsonify({'bmi': bmi}), 200
+
+        except ValueError:
+            # Obsługa błędu przy rzutowaniu na float
+            return jsonify({'error': 'Nieprawidłowe dane wejściowe. Podaj liczby!'}), 400
+
+    except Exception as e:
+        # Ogólna obsługa błędów
+        return jsonify({'error': f'Wystąpił nieoczekiwany błąd: {str(e)}'}), 500
+
+
+@procedure_bp.route('/search_icd10', methods=['GET', 'POST'])
+@login_required
+def search_icd10():
+    try:
+        if request.method == "POST":
+            search_term = request.json.get("icd10Code", None)
+
+            if not search_term:
+                return jsonify({'error': 'tekst jest wymagany!'}), 400
+
+            search_term = search_term.lower()
+
+            code = []
+            for paragraph in data_icd:
+                if search_term in paragraph.lower():
+                    code.append(paragraph)
+            if len(code) < 1:
+                code.append(search_term.upper() + ' - nie znaleziono !')
+
+            return jsonify({'result': code}), 200
+
     except TemplateNotFound:
         return abort(404)
--- app/utils/const.py
+++ app/utils/const.py
@@ -42,3 +42,95 @@ typ_messages_no = {
                     '9': "W badaniu laryngologicznym w dniu dzisiejszym stwierdzono przeciwwskazania laryngologiczne do pracy jako kierowca kategorii B.",
                     '10': "W badaniu laryngologicznym w dniu dzisiejszym stwierdzono przeciwwskazania laryngologiczne do pracy jako kierowca kategorii C+E.",
 }
+
+place = [
+    {'id': '1', 'name': 'Pruszcz Gdański'},
+    {'id': '2', 'name': 'Starogard Gdański'},
+    {'id': '3', 'name': 'Gdańsk'},
+    {'id': '4', 'name': 'Sopot'},
+    {'id': '5', 'name': 'Tczew'},
+    {'id': '6', 'name': 'Gdynia'}
+]
+
+
+W0 = {
+      'W01 -  48 pkt': 'Porada pohospitalizacyjna - do 30 dni od wypisu',
+      'W02 -  11 pkt': 'Porada receptowa',
+      'W11 -  44 pkt': 'Porada I typu: W1 / 2xW1',
+      'W12 -  75 pkt': 'Porada II typu: 3xW1 / W2',
+      'W13 - 133 pkt': 'Porada III typu: 3xW1 + W2 / W2 + W16 / 2xW2 / W3',
+      'W14 - 172 pkt': 'Porada IV typu: 3xW1 + W3 / 2xW3 / 2xW10',
+      'W15 -  56 pkt': 'Porada V typu: W16 (Histopatologia)',
+      'W17 - 104 pkt': 'Porada VII typu: W9 / W10 (USG Doppler)',
+      'W31 -  82 pkt': 'Porada udzielana w miejscu pobytu świadczeniobiorcy'
+}
+
+W1 = {
+      '20.391': 'Posturografia',
+      '21.293': 'Rinoskopia tylna',
+      '31.42': 'Laryngoskopia i inne wziernikowanie tchawicy',
+      '87.164': 'Rtg zatok nosa',
+      '87.165': 'Rtg nosa',
+      '95.413': 'Badanie odruchu strzemiączkowego',
+      '95.415': 'Tympanometria',
+      '95.45': 'Test obrotowy',
+      '95.48': 'Dopasowanie aparatu słuchowego',
+      '99.9960': 'Próby kaloryczne',
+      '99.9970': 'Gustometria swoista',
+      '99.9971': 'Elektrogustometria',
+      '99.9975': 'Olfaktometria podmuchowa',
+      '99.9976': 'Psychofizyczny test identyfikacji zapachów',
+      '99.9977': 'Badanie węchu zestawem markerów'
+}
+
+W2 = {
+      '87.092': 'Rtg krtani bez kontrastu – zdjęcia warstwowe',
+      '87.093': 'Rtg przewodu nosowo – łzowego z kontrastem',
+      '87.094': 'Rtg nosogardzieli bez kontrastu',
+      '87.095': 'Rtg gruczołów ślinowych bez kontrastu',
+      '87.098': 'Rtg gruczołów ślinowych z kontrastem',
+      '87.11': 'Rtg panoramiczne zębów',
+      '87.174': 'Rtg twarzoczaszki – przeglądowe',
+      '87.175': 'Rtg twarzoczaszki – celowane lub czynnościowe',
+      '87.176': 'Rtg czaszki – przeglądowe',
+      '87.177': 'Rtg czaszki – celowane lub czynnościowe',
+      '87.221': 'Rtg kręgosłupa odcinka szyjnego – przeglądowe',
+      '87.222': 'Rtg kręgosłupa odcinka szyjnego – celowane lub czynnościowe',
+      '87.440': 'Rtg klatki piersiowej',
+      '87.495': 'Rtg śródpiersia',
+      '87.496': 'Rtg tchawicy',
+      '87.691': 'Rtg przełyku z kontrastem',
+      '88.717': 'USG ślinianek',
+      '88.719': 'USG krtani',
+      '88.790': 'USG węzłów chłonnych',
+      '89.121': 'Rhinomanometria',
+      '91.821': 'Badanie materiału biologicznego – posiew jakościowy i ilościowy',
+      '91.831': 'Badanie materiału biologicznego – posiew jakościowy wraz z identyfikacją drobnoustroju',
+      '91.841': 'Badanie mikroskopowe materiału biologicznego – parazytologia',
+      '95.241': 'Elektronystagmogram (ENG)',
+      '95.242': 'Video ENG',
+      '95.412': 'Audiometria impedancyjna',
+      '95.414': 'Subiektywna audiometria',
+      '95.436': 'Otoemisja akustyczna',
+      '99.9955': 'Próby nadprogowe'
+}
+
+W3 = {
+      '29.1901': 'Stroboskopia',
+      '29.1902': 'Videostroboskopia',
+      '89.152': 'Somatosensoryczne potencjały wywołane (SEP)',
+      '89.153': 'Słuchowe potencjały wywołane z pnia mózgu (BAEP)',
+}
+
+W9 = {
+      '88.716': 'Badanie USG przezczaszkowe - Doppler'
+}
+
+W10 = {
+      '88.714': 'Badanie USG głównych naczyń szyi - Doppler'
+}
+
+W16 = {
+      '91.447': 'Badanie mikroskopowe materiału biologicznego – badanie cytologiczne',
+      '91.891': 'Badanie mikroskopowe materiału biologicznego – preparat bezpośredni'
+}
